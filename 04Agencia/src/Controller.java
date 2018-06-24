563e37726fcd1bdcbf61d2a2debf7caf780013ea
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:29:55 -0300

    Update Controller.java

--- 04Agencia/src/Controller.java
+++ 04Agencia/src/Controller.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Controller {
 		Repositorio<Cliente> usuarios;
 		Repositorio<Conta> contas;
@@ -20,30 +19,43 @@ public class Controller {
 		String[] ui = line.split(" ");
 
 		if (ui[0].equals("addCliente"))
+			//adiciona novo cliente na lista de usuarios
 			usuarios.add(ui[1], new Cliente(ui[1], ui[2]));
 		else if (ui[0].equals("showUsers")) {
 			String saida = "";
+			//para cada usuario dentro da lista de usuarios
 			for(Cliente us : usuarios.getAll())
+				//saida recebe usuarios
 				saida += us.getIdcliente() + "\n";
 			return saida;
 		}
 		else if(ui[0].equals("loginCliente")) {
+			//recebe nome e senha do usuario
 			ger.Login(ui[1], ui[2]);
+			
 		    ger.getUser().contas.add(""+ num , new Conta(num));
+		    
 		    num++;
+		    //esta logado 
             aux++;
 		}
 		else if(ui[0].equals("depositar"))
+			//deposita dinheiro na conta do usuario
 		    ger.getUser().contas.get(ui[1]).depositar(Float.parseFloat(ui[2]));	
 		else if(ui[0].equals("sacar"))
+			//tira dinheiro da conta do usuario
 			ger.getUser().contas.get(ui[1]).sacar(Float.parseFloat(ui[2]));
 		else if(ui[0].equals("encerrar")) {
+			//sair da conta
 			ger.getUser().contas.get(ui[1]).encerrar();
+			//n esta logado
 			aux--;
 		}
 		else if(ui[0].equals("saldo"))
+			//mostrar saldo do usuario
 			System.out.println(""+ger.getUser().contas.get(ui[1]).getSaldo());
 		else if(ui[0].equals("addConta")) {
+		//se usuario estiver logado
 		if(aux > 1)
 			throw new RuntimeException("fail: você possui o numero maximo de contas ativa");
 		ger.getUser().contas.add("" + num, new Conta(num));
@@ -51,6 +63,7 @@ public class Controller {
 		aux++;
 		}
 		else if(ui[0].equals("transferir"))
+			//pega dinheiro do usuario 1 e transferi para o outro usuario
 			ger.getUser().contas.get(ui[1]).transferir(Float.parseFloat(ui[2]), usuarios.get(ui[3]).contas.get(ui[4]));
 		    
 		else if(ui[0].equals("logout"))
@@ -80,7 +93,6 @@ public class Controller {
 	    }
 	  
 	}
-
 
 
 
