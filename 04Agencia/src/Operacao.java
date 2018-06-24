4b5c99a024478dd61461996420cd0c23ff72ee01
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:31:50 -0300

    Update Operacao.java

--- 04Agencia/src/Operacao.java
+++ 04Agencia/src/Operacao.java
@@ -1,38 +1,40 @@
 
 public class Operacao {
-
-	private String descricao;
-	private float valor;
-	private float saldoparcial = 0;
 	
-	public Operacao(String descricao, float valor) {
-		this.descricao = descricao;
-		this.valor = valor;
-		this.saldoparcial += valor;
-	}
+		//declarando variaveis
+		private String descricao;
+		private float valor;
+		private float saldoparcial = 0;
+		
+		//construtor
+		public Operacao(String descricao, float valor) {
+			this.descricao = descricao;
+			this.valor = valor;
+			this.saldoparcial += valor;
+		}
 	
-	
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public float getValor() {
-		return valor;
-	}
-	public void setValor(float valor) {
-		this.valor = valor;
-	}
-	public float getSaldoparcial() {
-		return saldoparcial;
-	}
-	public void setSaldoparcial(float saldoparcial) {
-		this.saldoparcial = saldoparcial;
-	}
-	
-	public String toString() {
-		return "" + descricao + " " + valor + " " + saldoparcial;
-	}
-	
-}
+			//getters e setters das variaveis da classe
+			public String getDescricao() {
+				return descricao;
+			}
+			public void setDescricao(String descricao) {
+				this.descricao = descricao;
+			}
+			public float getValor() {
+				return valor;
+			}
+			public void setValor(float valor) {
+				this.valor = valor;
+			}
+			public float getSaldoparcial() {
+				return saldoparcial;
+			}
+			public void setSaldoparcial(float saldoparcial) {
+				this.saldoparcial = saldoparcial;
+			}
+			
+			public String toString() {
+				return "" + descricao + " " + valor + " " + saldoparcial;
+			}
+			
+		}
