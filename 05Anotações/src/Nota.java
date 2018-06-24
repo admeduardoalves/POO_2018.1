225d2c87d3d7c6694a3eb898d5316ce473a28c18
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:43:25 -0300

    Update Nota.java

--- 05Anotações/src/Nota.java
+++ 05Anotações/src/Nota.java
@@ -1,31 +1,32 @@
-
 public class Nota {
 
-	private String titulo;
-	private String texto;
+		// declarando variaveis 
+		private String titulo;
+		private String texto;
+		
+		//construtor
+		public Nota(String titulo, String texto) {
+			this.titulo = titulo;
+			this.texto = texto;
+		}
 	
-	public Nota(String titulo, String texto) {
-		this.titulo = titulo;
-		this.texto = texto;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getTexto() {
-		return texto;
-	}
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
+		public String getTitulo() {
+			return titulo;
+		}
 	
-	public String toString() {
-		return titulo + ":" + texto;
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+	
+		public String getTexto() {
+			return texto;
+		}
+	
+		public void setTexto(String texto) {
+			this.texto = texto;
+		}
+		
+		public String toString() {
+			return titulo + ":" + texto;
+		}
 	}
-}
