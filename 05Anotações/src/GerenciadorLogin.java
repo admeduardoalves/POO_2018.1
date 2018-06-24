0143661ca890091aef9c9f879fb5ce65e2e0a385
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:42:31 -0300

    Update GerenciadorLogin.java

--- 05Anotações/src/GerenciadorLogin.java
+++ 05Anotações/src/GerenciadorLogin.java
@@ -1,34 +1,41 @@
 
+
 public class GerenciadorLogin {
 	
-
-	private Repositorio<User> usuarios;
-	private User user;
+		//declarando lista de usuarios
+		private Repositorio<User> usuarios;
+		private User user;
+		
+		//construtor
+		public GerenciadorLogin(Repositorio<User> usuarios) {
+			this.usuarios = usuarios;
+			user = null;
+		}
 	
-	public GerenciadorLogin(Repositorio<User> usuarios) {
-		this.usuarios = usuarios;
-		user = null;
-	}
-	
-	public void Login(String usuario, String password) {
-		if(user != null)
-			throw new RuntimeException("fail: Ja existe alguem logado");
-		if(!usuarios.get(usuario).matchPassword(password)) 
-		   throw new RuntimeException("fail: senha invalida ou pessoa nao encontrada");
-		this.user = usuarios.get(usuario);
-	}
-
-	public void Logout() {
-		if(user == null)
-		    throw new RuntimeException("fail: não está ninguem logado");
-		System.out.println("ok");
-		this.user = null;
-	}
-	
-	public User getUser() {
-		if(user == null)
-			throw new RuntimeException("fail: não está ninguem logado");
-		return user;
-	}
-	
+			public void Login(String usuario, String password) {
+				//se usuario estiver logado
+				if(user != null)
+					throw new RuntimeException("fail: Ja existe alguem logado");
+				// se n achar nome do usuario e senha 
+				if(!usuarios.get(usuario).matchPassword(password)) 
+				   throw new RuntimeException("fail: senha invalida ou pessoa nao encontrada");
+				//logar usuario
+				this.user = usuarios.get(usuario);
+			}
+		
+			public void Logout() {
+				//se usuario estiver deslogado
+				if(user == null)
+				    throw new RuntimeException("fail: não está ninguem logado");
+				System.out.println("ok");
+				//deslogar
+				this.user = null;
+			}
+			
+			public User getUser() {
+				if(user == null)
+					throw new RuntimeException("fail: não está ninguem logado");
+				return user;
+			}
+			
 }
