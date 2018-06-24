1bd40db9d33e4f9f469ecf7db55cd82a52da91b7
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:51:34 -0300

    Update Cliente.java

--- 06Clinica/src/Cliente.java
+++ 06Clinica/src/Cliente.java
@@ -1,10 +1,12 @@
 
 public class Cliente {
 
+	//declarando variaveis
 	private String idcliente;
 	private String nome;
 	Repositorio<Animal> animais;
 	
+	//construtor
 	public Cliente(String idcliente, String nome) {
 		this.idcliente = idcliente;
 		this.nome = nome;
@@ -18,7 +20,7 @@ public class Cliente {
 	public void setIdcliente(String idcliente) {
 		this.idcliente = idcliente;
 	}
-
+	//pegando lista de animais e retornando animais
 	public Repositorio<Animal> getAnimais() {
 		return animais;
 	}
@@ -33,7 +35,9 @@ public class Cliente {
 	
 	public String mostrarAnimais() {
 		String saida = " ";
+		//para cada aniaml dentro da lista de animais 
 		for(Animal a : animais.getAll())
+			//saida recebe animais
 			saida += a.toString() + "\n";
 		return saida;
 	}
