2a87d1d1b5fab586c75d0f9a1cc241df8bbf54ec
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:31:05 -0300

    Update GerenciadorLogin.java

--- 04Agencia/src/GerenciadorLogin.java
+++ 04Agencia/src/GerenciadorLogin.java
@@ -1,33 +1,38 @@
 public class GerenciadorLogin {
-
-	private Repositorio<Cliente> usuarios;
-	private Cliente user;
+		//declarando variaveis
+		private Repositorio<Cliente> usuarios;
+		private Cliente user;
+		
+		//construtor
+		public GerenciadorLogin(Repositorio<Cliente> usuarios) {
+			this.usuarios = usuarios;
+			user = null;
+		}
 	
-	
-	public GerenciadorLogin(Repositorio<Cliente> usuarios) {
-		this.usuarios = usuarios;
-		user = null;
-	}
-	
-	public void Login(String usuario, String password) {
-		if(user != null)
-			throw new RuntimeException("fail: Ja existe alguem logado");
-		if(!usuarios.get(usuario).matchPassword(password)) 
-		   throw new RuntimeException("fail: senha invalida ou pessoa nao encontrada");
-		this.user = usuarios.get(usuario);
-	}
-
-	public void Logout() {
-		if(user == null)
-		    throw new RuntimeException("fail: não está ninguem logado");
-		System.out.println("ok");
-		this.user = null;
-	}
-	
-	public Cliente getUser() {
-		if(user == null)
-			throw new RuntimeException("fail: não está ninguem logado");
-		return user;
-	}
-	
-}
+			public void Login(String usuario, String password) {
+				//se usuario for diferente de nulo
+				if(user != null)
+					throw new RuntimeException("fail: Ja existe alguem logado");
+				//se senha e nome do usario nã exitirem na lista de usuarios 
+				if(!usuarios.get(usuario).matchPassword(password)) 
+				   throw new RuntimeException("fail: senha invalida ou pessoa nao encontrada");
+				//loga usuario
+				this.user = usuarios.get(usuario);
+			}
+		
+			public void Logout() {
+				//se usuario for nulo
+				if(user == null)
+				    throw new RuntimeException("fail: não está ninguem logado");
+				System.out.println("ok");
+				//desloga
+				this.user = null;
+			}
+			
+			public Cliente getUser() {
+				if(user == null)
+					throw new RuntimeException("fail: não está ninguem logado");
+				return user;
+			}
+			
+		}
