a7c951447e5424789abb849cfd7063976c4ac59e
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-24 00:06:05 -0300

    Update Gerenciador.java

--- 07Twitter/src/Gerenciador.java
+++ 07Twitter/src/Gerenciador.java
@@ -1,29 +1,34 @@
 
 public class Gerenciador {
-
-	private Repositorio<Tweet> tweets;
-	
-	
-	public Gerenciador() {
-		tweets = new Repositorio<Tweet>("tweets");
+		//criando variavel repositorio do tp tweet com nome tweets
+		private Repositorio<Tweet> tweets;
+		
+		// criando construtor
+		public Gerenciador() {
+			//criando gerador de tweets para armazenar tweets  
+			tweets = new Repositorio<Tweet>("tweets");
+		}
+		//pegando tweets armazenados 
+		public Repositorio<Tweet> getTweets() {
+			return tweets;
+		}
+		// definindo tweet armazenados
+		public void setTweets(Repositorio<Tweet> tweets) {
+			this.tweets = tweets;
+		}
+		//gerar tweet
+		public void gerarTweet(Tweet t) {
+			//adiciona um tweet recebendo id do tweet 
+			this.tweets.add(""+t.getIdTweet(), t);
+		}
+		//mostar tweets
+		public String showTweets() {
+			String saida = "";
+			//para cada tweet dentro do repositorio de tweets
+			for(Tweet t : tweets.getAll())
+				//saida recebe tweet
+				saida += t.toString() + "\n";
+			//retorna saida
+			return saida;
+		}
 	}
-
-	public Repositorio<Tweet> getTweets() {
-		return tweets;
-	}
-
-	public void setTweets(Repositorio<Tweet> tweets) {
-		this.tweets = tweets;
-	}
-	
-	public void gerarTweet(Tweet t) {
-		this.tweets.add(""+t.getIdTweet(), t);
-	}
-	
-	public String showTweets() {
-		String saida = "";
-		for(Tweet t : tweets.getAll())
-			saida += t.toString() + "\n";
-		return saida;
-	}
-}
