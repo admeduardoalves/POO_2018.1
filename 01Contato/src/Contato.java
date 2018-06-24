1bec725d6bd3492c226028795864a10f4dea639d
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 22:30:49 -0300

    Update Contato.java

--- 01Contato/src/Contato.java
+++ 01Contato/src/Contato.java
@@ -1,47 +1,58 @@
-import java.util.ArrayList;
+java.util.ArrayList;
 import java.util.List;
 
 public class Contato {
-	private String nome;
-	private List<Telefone> telefones;
+		//criando variavel nome
+		private String nome;
+		//criando lista de telefones do tp telefone
+		private List<Telefone> telefones;
+		
+		//contrutor
+		public Contato(String nome) {
+			//recebendo valores
+			this.nome = nome;
+			this.telefones = new ArrayList<>();
+		}
+		//criando getters e settters das variaveis
+		public String getNome() {
+			return this.nome;
+		}
+		
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public List<Telefone> getTelefones() {
+			return telefones;
+		}
 	
-	public Contato(String nome) {
-		this.nome = nome;
-		this.telefones = new ArrayList<>();
-	}
-	
-	public String getNome() {
-		return this.nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public List<Telefone> getTelefones() {
-		return telefones;
-	}
-	
-	public boolean addTelefone(Telefone telefone) throws Exception {
-		for (Telefone fone : telefones) {
-			if(fone.foneId.equals(telefone.getFoneId())) {
-				throw new Exception("Telefone já existe");
+			//adcionar telefone
+			public boolean addTelefone(Telefone telefone) throws Exception {
+				//para cada telefone dentro da lista de telefones
+				for (Telefone fone : telefones) {
+					//se telefone eu achar o telefone
+					if(fone.foneId.equals(telefone.getFoneId())) {
+						throw new Exception("Telefone já existe");
+					}
+				}
+				// adiciona telefone na lista de telefones
+				return this.telefones.add(telefone);
+			}
+			//remover telefone
+			public boolean rmTelefone(String foneId) throws Exception{
+				// para cada telefone dentro da lista de telefones
+				for (Telefone fone : telefones) {
+					// se achar telefone
+					if (fone.foneId.equals(foneId)) {
+						// remove telefone da lista de telefones
+						return this.telefones.remove(fone);
+					}
+				}
+				throw new Exception("Identificador não encontrado");
+			}
+			
+			@Override
+			public String toString() {
+				return "Nome: " + this.nome + " FoneId: " + this.telefones;
 			}
 		}
-		return this.telefones.add(telefone);
-	}
-	
-	public boolean rmTelefone(String foneId) throws Exception{
-		for (Telefone fone : telefones) {
-			if (fone.foneId.equals(foneId)) {
-				return this.telefones.remove(fone);
-			}
-		}
-		throw new Exception("Identificador não encontrado");
-	}
-	
-	@Override
-	public String toString() {
-		return "Nome: " + this.nome + " FoneId: " + this.telefones;
-	}
-}
