d0d7a8af77be2b7fb1a24a50c7693525f5b9a6e0
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 22:32:53 -0300

    Update Controller.java

--- 01Contato/src/Controller.java
+++ 01Contato/src/Controller.java
@@ -5,23 +5,29 @@ public class Controller {
 		Agenda agenda = new Agenda();
 		
 		while (true) {
-			String line = Input.input("Digite um comando: ");
+			String line = input.input("Digite um comando: ");
 			System.out.println(line);
-			ArrayList<String> userInput = Input.split(line, " ");
+			ArrayList<String> userInput = input.split(line, " ");
 			
 			try {
 				if(userInput.get(0).equals("addContato")) {
+					//adicionar novo contato a agenda
 					agenda.addContato(new Contato(userInput.get(1)));
 				}else if (userInput.get(0).equals("rmContato")) {
+					//remover contato da agenda
 					agenda.rmContato(userInput.get(1));
 				}else if (userInput.get(0).equals("seach")) {
+					//procurar contato dentro da agenda
 					agenda.seach(userInput.get(1));
 				}else if (userInput.get(0).equals("addFone")) {
+					//adicionar novo telefone dentro da agenda
 					agenda.addTelefone(userInput.get(1), new Telefone(userInput.get(2), 
-														Input.toInt(userInput.get(3))));
+														input.toInt(userInput.get(3))));
 				}else if (userInput.get(0).equals("rmFone")) {
+					//remover telefone da agenda
 					agenda.rmTelefone(userInput.get(1), userInput.get(2));
 				}else if (userInput.get(0).equals("fim")) {
+					//sair 
 					break;
 				}
 			}catch(Exception e) {
