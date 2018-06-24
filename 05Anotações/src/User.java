d7ccd0b3f1835da061a01b4f3b3d0afeca0f8613
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:44:47 -0300

    Update User.java

--- 05Anotações/src/User.java
+++ 05Anotações/src/User.java
@@ -1,53 +1,49 @@
-
-
 public class User implements Comparable<User>{
-
-	private String username;
-	private String password;
-	public Repositorio<Nota> notas;
 	
-	public Repositorio<Nota> getNotas() {
-		return notas;
-	}
-
-	public void setNotas(Repositorio<Nota> notas) {
-		this.notas = notas;
-	}
-
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-		notas = new Repositorio<Nota>(username);
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public boolean matchPassword(String password) {
-		return this.password.equals(password);
-	}
-	
-	public String toString() {
-		return username + ":" + password;
-	}
-
-	
-	public int compareTo(User other) {
-		return this.username.compareTo(other.username);
-	}
-
-
-
-}
-
-
-
+		//declarando variaveis
+		private String username;
+		private String password;
+		public Repositorio<Nota> notas;
+		//pegando e retornando notas
+		public Repositorio<Nota> getNotas() {
+			return notas;
+		}
+		//definido notas
+		public void setNotas(Repositorio<Nota> notas) {
+			this.notas = notas;
+		}
+		//construtor
+		public User(String username, String password) {
+			this.username = username;
+			this.password = password;
+			notas = new Repositorio<Nota>(username);
+		}
+			//getters e setters das variaveis da classe
+			public String getUsername() {
+				return username;
+			}
+		
+			public void setUsername(String username) {
+				this.username = username;
+			}
+		
+			public void setPassword(String password) {
+				this.password = password;
+			}
+			
+			public boolean matchPassword(String password) {
+				return this.password.equals(password);
+			}
+			
+			public String toString() {
+				return username + ":" + password;
+			}
+		
+			//saber se tem outro usuario como msm nome
+			public int compareTo(User other) {
+				return this.username.compareTo(other.username);
+			}
+		
+		
+		
+		}
