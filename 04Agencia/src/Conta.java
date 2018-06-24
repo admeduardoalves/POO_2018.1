5b2c1a6e584295d5ac5e4acbb06b73632a933926
admeduardoalves <38011591+admeduardoalves@users.noreply.github.com>
2018-06-23 23:28:40 -0300

    Update Conta.java

--- 04Agencia/src/Conta.java
+++ 04Agencia/src/Conta.java
@@ -1,83 +1,97 @@
 
 public class Conta {
-
-	private float saldo;
-	private int numero;
-	private Repositorio<Operacao> extrato;
-	private boolean ativa;
-	int auxcontadorextrato;
-	
-	
-	public Conta(int numero) {
-		this.numero = numero;
-		this.saldo += saldo;
-		this.extrato = new Repositorio<Operacao>(null);
-		this.ativa =  true;
-	}
-
-	public float getSaldo() {
-		return saldo;
-	}
-
-	public float setSaldo(float saldo) {
-		return this.saldo = saldo;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public Repositorio<Operacao> getExtrato() {
-		return extrato;
-	}
-
-	public boolean isAtiva() {
-		return ativa;
-	}
-
-	public void setAtiva(boolean ativa) {
-		this.ativa = ativa;
-	}
-	
-	public void sacar(float value) {
-		if(ativa) {
-		     if(value < 0 && value > saldo)
-			     throw new RuntimeException("fail: valor " + value + " menor que 0" );
-		     this.setSaldo(saldo -= value);
-		     this.extrato.add( "" + auxcontadorextrato , new Operacao("sacar", value) );
-		     auxcontadorextrato++;
-		     return;
+		//declarando variaveis
+		private float saldo;
+		private int numero;
+		//declarando uma variavel do tp operação 
+		private Repositorio<Operacao> extrato;
+		private boolean ativa;
+		int auxcontadorextrato;
+		
+		//construtor
+		public Conta(int numero) {
+			this.numero = numero;
+			this.saldo += saldo;
+			this.extrato = new Repositorio<Operacao>(null);
+			this.ativa =  true;
 		}
-		throw new RuntimeException("fail: Conta não ativa");
-	}
 	
-	public void depositar(float value) {
-		if (ativa) {
-			if (value < 0)
-				throw new RuntimeException("fail: valor " + value + "menor que  0");
-			this.extrato.add("" + auxcontadorextrato, new Operacao("depositar", value));
-			this.saldo += value;
-			auxcontadorextrato++;
-		    return;
+		public float getSaldo() {
+			return saldo;
 		}
-		throw new RuntimeException("fail: Conta não ativa");
-	}
 	
-	public void transferir(float value, Conta other) {
-		if (this.ativa) {
-			if (value < 0)
-				throw new RuntimeException("fail: valor" + value + "menor que 0");
-			this.extrato.add("" + auxcontadorextrato, new Operacao("transferencia", value));
-			this.saldo -= value;
-			other.saldo += value;
-			auxcontadorextrato++;
-			return;
+		public float setSaldo(float saldo) {
+			return this.saldo = saldo;
 		}
-		throw new RuntimeException("fail: Conta não ativa");
-	}
 	
-	public void encerrar() {
-		this.ativa = false;
-	}
+		public int getNumero() {
+			return numero;
+		}
+		//pegando extrato e mostrando extrato
+		public Repositorio<Operacao> getExtrato() {
+			return extrato;
+		}
 	
-}
+		public boolean isAtiva() {
+			return ativa;
+		}
+	
+		public void setAtiva(boolean ativa) {
+			this.ativa = ativa;
+		}
+	
+			public void sacar(float value) {
+				// se conta estiver ativa
+				if(ativa) {
+					//se valor for menor que zero ou maior que o saldo
+				     if(value < 0 && value > saldo)
+					     throw new RuntimeException("fail: valor " + value + " menor que 0" );
+				     //saldo - saque
+				     this.setSaldo(saldo -= value);
+				     //adiciona nova operação no extrato da conta
+				     this.extrato.add( "" + auxcontadorextrato , new Operacao("sacar", value) );
+				     auxcontadorextrato++;
+				     return;
+				}
+				throw new RuntimeException("fail: Conta não ativa");
+			}
+			
+			public void depositar(float value) {
+				//se conta estiver ativa 
+				if (ativa) {
+					//se valor for menor que 0
+					if (value < 0)
+						throw new RuntimeException("fail: valor " + value + "menor que  0");
+					//adiciona nova operação no extrato da conta
+					this.extrato.add("" + auxcontadorextrato, new Operacao("depositar", value));
+					//saldo + deposito
+					this.saldo += value;
+					auxcontadorextrato++;
+				    return;
+				}
+				throw new RuntimeException("fail: Conta não ativa");
+			}
+			
+			public void transferir(float value, Conta other) {
+				//se conta estiver ativa 
+				if (this.ativa) {
+					//se valor da conta for 0
+					if (value < 0)
+						throw new RuntimeException("fail: valor" + value + "menor que 0");
+					//adiciona nova operação no extrato da conta
+					this.extrato.add("" + auxcontadorextrato, new Operacao("transferencia", value));
+					//saldo - valor
+					this.saldo -= value;
+					//saldo do outro + valor
+					other.saldo += value;
+					auxcontadorextrato++;
+					return;
+				}
+				throw new RuntimeException("fail: Conta não ativa");
+			}
+			
+			public void encerrar() {
+				this.ativa = false;
+			}
+			
+		}
